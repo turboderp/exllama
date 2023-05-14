0deaf64195e5ca9d063181e693114852c9a4d18d
emosuka <flemming@optur.net>
2023-05-14 03:54:37 +0200

    Test code for another (4K tuned) model

--- test_chatbot.py
+++ test_chatbot.py
@@ -14,28 +14,39 @@ torch.cuda._lazy_init()
 # model_path = "/mnt/Fast/models/llama-7b-4bit-128g/llama-7b-4bit-128g.safetensors"
 # model_groupsize = 128
 # break_on_newline = True
-#
+# model_seq_len = 2048
+
 # tokenizer_model_path = "/mnt/Fast/models/llama-13b-4bit-128g/tokenizer.model"
 # model_config_path = "/mnt/Fast/models/llama-13b-4bit-128g/config.json"
 # model_path = "/mnt/Fast/models/llama-13b-4bit-128g/llama-13b-4bit-128g.safetensors"
 # model_groupsize = 128
 # break_on_newline = True
+# model_seq_len = 2048
 
 tokenizer_model_path = "/mnt/Fast/models/llama-30b-4bit-128g/tokenizer.model"
 model_config_path = "/mnt/Fast/models/llama-30b-4bit-128g/config.json"
 model_path = "/mnt/Fast/models/llama-30b-4bit-128g/llama-30b-4bit-128g.safetensors"
 model_groupsize = 128
 break_on_newline = True
+model_seq_len = 2048
 
 # tokenizer_model_path = "/mnt/Fast/models/gpt4all-13b-4bit-128g/tokenizer.model"
 # model_config_path = "/mnt/Fast/models/gpt4all-13b-4bit-128g/config.json"
 # model_path = "/mnt/Fast/models/gpt4all-13b-4bit-128g/gpt4all-13b-4bit-128g.safetensors"
 # model_groupsize = 128
 # break_on_newline = False
+# model_seq_len = 2048
+
+# tokenizer_model_path = "/mnt/Fast/models/bluemoon-4k-13b-4bit-128g/tokenizer.model"
+# model_config_path = "/mnt/Fast/models/bluemoon-4k-13b-4bit-128g/config.json"
+# model_path = "/mnt/Fast/models/bluemoon-4k-13b-4bit-128g/bluemoonrp-13b-4k-epoch6-4bit-128g.safetensors"
+# model_groupsize = 128
+# break_on_newline = True
+# model_seq_len = 4096
 
 config = ExLlamaConfig(model_config_path)
 config.model_path = model_path
-config.max_seq_len = 2048
+config.max_seq_len = model_seq_len
 config.groupsize = model_groupsize
 model = ExLlama(config)
 cache = ExLlamaCache(model)
@@ -50,7 +61,7 @@ generator.settings.top_k = 20
 generator.settings.top_p = 0.65
 generator.settings.min_p = 0.02
 generator.settings.token_repetition_penalty_max = 1.15
-generator.settings.token_repetition_penalty_sustain = 50
+generator.settings.token_repetition_penalty_sustain = 128
 generator.settings.token_repetition_penalty_decay = 50
 
 # Be nice to Chatbort
@@ -63,11 +74,24 @@ past = f"This is a conversation between a guy called {username} and a quirky, cu
     f"{username}: Hi, {bot_name}!\n" \
     f"{bot_name}: Hello, {username}.\n"
 
+
+# GTP4ALL
+
 # username = "User"
 # bot_name = "Chatbort"
 # past = f"{username}: Hi, {bot_name}!\n" \
 #        f"{bot_name}: Hello, {username}.\n"
 
+
+# Bluemoon (how is this model meant to work?)
+
+# username = "ASSOCIATE"
+# bot_name = "LEAD"
+# past = "A transcript of a roleplay between two players, LEAD and ASSOCIATE. LEAD sets up a scenario and the characters, from " \
+#     "which ASSOCIATE then assumes a character role and continues the story for that role in response to description given " \
+#     "by LEAD. The story and characters are developed by exchange of detailed event descriptions and character dialogs, " \
+#     "successively given by both LEAD and ASSOCIATE.\n" \
+#     "LEAD: Your phone is ringing.\n"
 
 max_response_tokens = 256
 extra_prune = 256
