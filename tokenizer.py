0fae40bed63228b027de6826f755cfb7cd296a30
SinanAkkoyun <akkoyunsinan2@gmx.de>
2023-07-25 23:55:08 +0200

    fix inconsistent indentation

--- tokenizer.py
+++ tokenizer.py
@@ -67,9 +67,9 @@ class ExLlamaTokenizer:
             # pad bos and eos
 
             if add_bos:
-              ids = [self.bos_token_id] + ids
+                ids = [self.bos_token_id] + ids
             if add_eos:
-              ids = ids + [self.eos_token_id]
+                ids = ids + [self.eos_token_id]
 
             stacked_ids = torch.tensor(ids).unsqueeze(0)
 
