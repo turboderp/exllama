30ef9d0256626c3b8bd237db13562a630083669f
Teresa <85707358+Kerushii@users.noreply.github.com>
2023-08-18 00:05:20 -0700

    Add files via upload

---
+++ example_ws.py
@@ -0,0 +1,323 @@
+import asyncio
+import websockets
+import json
+from sentencepiece import SentencePieceProcessor
+
+from model import ExLlama, ExLlamaCache, ExLlamaConfig
+from lora import ExLlamaLora
+from tokenizer import ExLlamaTokenizer
+from generator import ExLlamaGenerator
+import argparse
+import torch
+import sys
+import os
+import glob
+import model_init
+
+# Initialized from command line args by init()
+
+model: ExLlama
+cache: ExLlamaCache
+config: ExLlamaConfig
+generator: ExLlamaGenerator
+tokenizer: ExLlamaTokenizer
+max_cached_strings = 100
+tokenizer_cache = {}
+
+
+prompt_ids: torch.tensor
+stop_strings: list
+stop_tokens: list
+held_text: str
+max_stop_string: int
+remaining_tokens: int
+
+full_prompt: str
+utilized_prompt: str
+built_response: str
+
+def cached_tokenize(text: str):
+    global model, cache, config, generator, tokenizer
+    global max_cached_strings, tokenizer_cache
+
+    if text in tokenizer_cache:
+        return tokenizer_cache[text]
+
+    while len(tokenizer_cache) >= max_cached_strings:
+        del tokenizer_cache[next(iter(tokenizer_cache))]  # Always removes oldest entry as of Python 3.7
+
+    new_enc = tokenizer.encode(text)
+    tokenizer_cache[text] = new_enc
+    return new_enc
+
+def begin_stream(prompt: str, stop_conditions: list, max_new_tokens: int, gen_settings: ExLlamaGenerator.Settings):
+    global model, cache, config, generator, tokenizer
+    global stop_strings, stop_tokens, prompt_ids, held_text, max_stop_string, remaining_tokens
+    global full_prompt, utilized_prompt, built_response
+
+    # Tokenize prompt and limit length to allow prompt and (max) new tokens within max sequence length
+
+    max_input_tokens = model.config.max_seq_len - max_new_tokens
+    input_ids = cached_tokenize(prompt)
+    input_ids = input_ids[:, -max_input_tokens:]
+    prompt_ids = input_ids
+
+    full_prompt = prompt
+    utilized_prompt = tokenizer.decode(prompt_ids)[0]
+    built_response = ""
+
+    remaining_tokens = max_new_tokens
+
+    # Settings
+
+    stop_strings = []
+    stop_tokens = []
+    for t in stop_conditions:
+        if isinstance(t, int): stop_tokens += [t]
+        if isinstance(t, str): stop_strings += [t]
+
+    held_text = ""
+
+    max_stop_string = 2
+    for ss in stop_strings:
+        max_stop_string = max(max_stop_string, get_num_tokens(ss) + 2)
+
+    generator.settings = gen_settings
+
+    # Start generation
+
+    generator.gen_begin_reuse(input_ids)
+
+def stream():
+    global model, cache, config, generator, tokenizer
+    global stop_strings, stop_tokens, prompt_ids, held_text, max_stop_string, remaining_tokens
+    global full_prompt, utilized_prompt, built_response
+
+    # Check total response length
+
+    if remaining_tokens == 0:
+        return held_text, True, full_prompt + built_response, utilized_prompt + built_response, built_response
+    remaining_tokens -= 1
+
+    # Generate
+
+    old_tail = tokenizer.decode(generator.sequence_actual[:, -max_stop_string:])[0]
+    next_token = generator.gen_single_token()
+
+    # End on stop token
+
+    if next_token in stop_tokens:
+        return held_text, True, full_prompt + built_response, utilized_prompt + built_response, built_response
+
+    # Get new text
+
+    new_tail = tokenizer.decode(generator.sequence_actual[:, -(max_stop_string + 1):])[0]
+    added_text = new_tail[len(old_tail):]
+    held_text += added_text
+
+    # Hold text if it's part of a stop condition, end if it's a full stop condition
+
+    partial_ss = False
+    for ss in stop_strings:
+
+        # Check if held_text fully contains stop string
+
+        position = held_text.find(ss)
+        if position != -1:
+            built_response += held_text[:position]
+            return held_text[:position], True, full_prompt + built_response, utilized_prompt + built_response, built_response
+
+        # Check if end of held_text overlaps with start of stop string
+
+        overlap = 0
+        for j in range(1, min(len(held_text), len(ss)) + 1):
+            if held_text[-j:] == ss[:j]: overlap = j
+        if overlap > 0: partial_ss = True
+
+    # Return partial result
+
+    if partial_ss:
+        return "", False, full_prompt + built_response, utilized_prompt + built_response, built_response
+
+    stream_text = held_text
+    held_text = ""
+    built_response += stream_text
+    return stream_text, False, full_prompt, utilized_prompt, built_response
+
+def oneshot_generation(prompt: str, stop_conditions: list, max_new_tokens: int, gen_settings: ExLlamaGenerator.Settings):
+
+    begin_stream(prompt, stop_conditions, max_new_tokens, gen_settings)
+    response = ""
+    while True:
+        _, eos, _, _, _ = stream()
+        if eos: break
+
+    return full_prompt + built_response, utilized_prompt + built_response, built_response
+
+
+    global model, cache, config, generator, tokenizer
+
+    # Global initialization
+
+    torch.set_grad_enabled(False)
+    torch.cuda._lazy_init()
+
+    # Parse arguments
+
+    parser = argparse.ArgumentParser(description = "Endpoint example")
+
+    model_init.add_args(parser)
+
+    parser.add_argument("-lora", "--lora", type = str, help = "Path to LoRA binary to use during benchmark")
+    parser.add_argument("-loracfg", "--lora_config", type = str, help = "Path to LoRA config to use during benchmark")
+    parser.add_argument("-ld", "--lora_dir", type = str, help = "Path to LoRA config and binary. to use during benchmark")
+
+    args = parser.parse_args()
+    model_init.post_parse(args)
+    model_init.get_model_files(args)
+
+    print_opts = []
+    model_init.print_options(args, print_opts)
+
+    # Paths
+
+    if args.lora_dir is not None:
+        args.lora_config = os.path.join(args.lora_dir, "adapter_config.json")
+        args.lora = os.path.join(args.lora_dir, "adapter_model.bin")
+
+    # Model globals
+
+    model_init.set_globals(args)
+
+    # Instantiate model and generator
+
+    config = model_init.make_config(args)
+
+    model = ExLlama(config)
+    cache = ExLlamaCache(model)
+    tokenizer = ExLlamaTokenizer(args.tokenizer)
+
+    model_init.print_stats(model)
+
+    # Load LoRA
+
+    lora = None
+    if args.lora:
+        print(f" -- LoRA config: {args.lora_config}")
+        print(f" -- Loading LoRA: {args.lora}")
+        if args.lora_config is None:
+            print(f" ## Error: please specify lora path to adapter_config.json")
+            sys.exit()
+        lora = ExLlamaLora(model, args.lora_config, args.lora)
+        if lora.bias_ignored:
+            print(f" !! Warning: LoRA zero bias ignored")
+
+    # Generator
+
+    generator = ExLlamaGenerator(model, tokenizer, cache)
+
+def get_num_tokens(text: str):
+
+    return cached_tokenize(text).shape[-1]
+
+
+
+
+# Websocket server
+async def estimateToken(request, ws):
+    text = request["text"]
+    numTokens=get_num_tokens(text)
+    return numTokens# return number of tokens in int
+
+async def oneShotInfer(request, ws):
+    stopToken = request["stopToken"]
+    fullContext = request["text"]
+    maxNew = int(request["maxNew"])
+    top_p = float(request["top_p"])
+    top_k = int(request["top_k"])
+    temp = float(request["temp"])
+    rep_pen = float(request["rep_pen"])
+    sc = [tokenizer.eos_token_id]
+    sc.append(stopToken)
+
+    gs = ExLlamaGenerator.Settings()
+    gs.top_k = top_k
+    gs.top_p = top_p
+    gs.temperature = temp
+
+    full_ctx, util_ctx, response = oneshot_generation(prompt=fullContext, stop_conditions=sc, max_new_tokens=maxNew, gen_settings=gs)
+
+    return full_ctx, util_ctx, response# return requested prompt/context, pruned prompt/context(eg. prunedctx+maxNew=4096), model generated response, not including prompt
+
+async def streamInfer(request, ws):
+    stopToken = [tokenizer.eos_token_id]
+    stopToken.append(request["stopToken"])
+    prompt = request["text"]
+    maxNew = int(request["maxNew"])
+    top_p = float(request["top_p"])
+    top_k = int(request["top_k"])
+    temp = float(request["temp"])
+    rep_pen = float(request["rep_pen"])
+    gs = ExLlamaGenerator.Settings()
+    gs.top_k = top_k
+    gs.top_p = top_p
+    gs.temperature = temp
+    begin_stream(prompt, stopToken, 10, gs)
+    while True:
+        chunk, eos, x, y, z = stream()
+        await ws.send(json.dumps({'action':'streamInfer',
+                                  'request_id':request['request_id'], 
+                                  'response':chunk,
+                                  'fullContext':x, 
+                                  'utilContext':y, 
+                                  'response':z}))
+        if eos: break
+    return prompt, y
+
+
+async def main(websocket, path):
+    async for message in websocket:
+        #try:
+            request = json.loads(message)
+            reqID = request["request_id"]
+            action = request["action"]
+
+            if action == "estimateToken":
+                response = await estimateToken(request, websocket)
+                await websocket.send(json.dumps({'action':action, 'request_id':reqID, 'response':response}))
+
+            if action == "echo":
+                await websocket.send(json.dumps({'action':action, 'request_id':reqID}))
+
+            elif action == "oneShotInfer":
+                fctx, utlctx, res = await oneShotInfer(request, websocket)
+                await websocket.send(json.dumps({'action':action, 'request_id':reqID, 'fullContext':fctx, 'utilContext':utlctx, 'response':res}))
+
+            elif action == "streamInfer":
+                fctx, utlctx= await streamInfer(request, websocket)
+                await websocket.send(json.dumps({'action':action, 'request_id':reqID, 'fullContext':fctx, 'utilContext':utlctx, 'response':'</s>'}))
+
+        #except Exception as e:
+            #print({"error": str(e)})
+
+model_directory = "./models/Llama-2-70B-chat-GPTQ/"
+
+tokenizer_path = os.path.join(model_directory, "tokenizer.model")
+model_config_path = os.path.join(model_directory, "config.json")
+st_pattern = os.path.join(model_directory, "*.safetensors")
+model_path = glob.glob(st_pattern)[0]
+esTokenizer = SentencePieceProcessor(model_file = tokenizer_path)
+config = ExLlamaConfig(model_config_path)               # create config from config.json
+config.set_auto_map('18.8897,18.8897')
+config.model_path = model_path                          # supply path to model weights file
+
+model = ExLlama(config)                                 # create ExLlama instance and load the weights
+print(f"Model loaded: {model_path}")
+
+tokenizer = ExLlamaTokenizer(tokenizer_path)            # create tokenizer from tokenizer model file
+cache = ExLlamaCache(model)                             # create cache for inference
+generator = ExLlamaGenerator(model, tokenizer, cache)   # create generator
+start_server = websockets.serve(main, "0.0.0.0", 8080)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
